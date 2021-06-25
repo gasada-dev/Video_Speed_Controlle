3a6347c9c3e221313970e99040c3cfda64fec742
gasada <gasada@protonmail.com>
2021-06-25 23:29:56 +0300

    feat: add normalize.scss, layout, scss code and start index.js

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+const SPEED = document.querySelectorAll('.speed')
+const BAR = document.querySelectorAll('.speed__bar')
+const VIDEO = document.querySelectorAll('.video')
+
+SPEED.addEventListener('mousemove', )
