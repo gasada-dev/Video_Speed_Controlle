b2713d3510676b2a507a0f8eb38d79c146ec29e0
gasada <gasada@protonmail.com>
2021-06-27 00:56:51 +0300

    feat: add func move, edit style

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
-const SPEED = document.querySelectorAll('.speed')
-const BAR = document.querySelectorAll('.speed__bar')
-const VIDEO = document.querySelectorAll('.video')
+const SPEED = document.querySelector('.speed')
+const BAR = document.querySelector('.speed__bar')
+const VIDEO = document.querySelector('.video')
 
-SPEED.addEventListener('mousemove', )
+function move(e) {
+  const I = e.pageY - this.offsetTop;
+  const PERCENT = I / this.offsetHeight;
+  const MIN = 0.5;
+  const MAX = 2;
+  const HEIGHT = Math.round(PERCENT * 100) + '%';
+  const playbackRate = PERCENT * (MAX - MIN) + MIN;
+
+  BAR.style.height = HEIGHT;
+  BAR.textContent = playbackRate.toFixed(2) + '×';
+  VIDEO.playbackRate = playbackRate;
+}
+
+SPEED.addEventListener('mousemove', move)
